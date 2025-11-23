cdce9a73112028040d7a6e7cdd709eabed6f1ebd
Francis Santiago <>
2025-11-23 20:21:10 -0300

    :sparkles: Adicionado hash de senha ao criar e atualizar estudantes

--- backend/src/students/students.service.ts
+++ backend/src/students/students.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { InjectModel } from '@nestjs/sequelize';
 import { Student } from '../models/student.model';
 
@@ -10,6 +11,9 @@ export class StudentsService {
   ) {}
 
   async create(studentData: Partial<Student>): Promise<Student> {
+    if (studentData.password) {
+      studentData.password = await bcrypt.hash(studentData.password, 10);
+    }
     return this.studentModel.create(studentData);
   }
 
@@ -24,6 +28,9 @@ export class StudentsService {
   async update(id: number, studentData: Partial<Student>): Promise<Student | null> {
     const student = await this.findOne(id);
     if (!student) return null;
+    if (studentData.password) {
+      studentData.password = await bcrypt.hash(studentData.password, 10);
+    }
     return student.update(studentData);
   }
 
